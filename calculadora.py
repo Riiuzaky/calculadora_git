2c08f150f240cdae04a98ce2dea3c5b63601499d
Riiuzaky <jonathans.cruzm@gmail.com>
2021-03-25 10:39:17 -0500

    Agregada la operacion resta y el esqueleto de multiplicacion

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,18 @@
 class Calculadora:
+    """"clase para las operaciones basicas""""
     def _init_(self):
         self.valor_1 = 0
         self.valor_2 = 0
         self.resultado = 0
 
         def sumar(self):
+            """"operacion que suma los valores seteados en la calculadora""""
             self.resultado = self.valor_1 + self.valor_2
+
+        def restar(self):
+            """"operacion que resta los valores seteados en la calculadora""""
+            self.resultado = self.valor_1 - self.valor_2
+
+        def multiplicar(self):
+            """"operacion que multiplica los valores seteados en la calculadora""""
+            pass
