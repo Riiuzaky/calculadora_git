9a73e78a3c2989b6455d90294d93541b4a986efb
Riiuzaky <jonathans.cruzm@gmail.com>
2021-03-25 11:24:18 -0500

    agregado endpoint para las operaciones

--- app.py
+++ app.py
@@ -13,7 +13,44 @@ def suma(valor_1, valor_2):
     c.valor_1 = valor_1
     c.valor_2 = valor_2
     c.sumar()
-    return (str(c.valor_1)+"+"+ str(c.valor_2)+"="+ str(c.resultado))
+    return (str(c.valor_1)+" + "+ str(c.valor_2)+"="+ str(c.resultado))
+
+@app.route('/resta/<int:valor_1>/<int:valor_2>')
+def resta(valor_1, valor_2):
+    c = Calculadora()
+    c.valor_1 = valor_1
+    c.valor_2 = valor_2
+    c.restar()
+    return (str(c.valor_1)+" - "+ str(c.valor_2)+"="+ str(c.resultado))
+
+@app.route('/multiplicacion/<int:valor_1>/<int:valor_2>')
+def multiplicacion(valor_1, valor_2):
+    c = Calculadora()
+    c.valor_1 = valor_1
+    c.valor_2 = valor_2
+    c.multiplicar()
+    return (str(c.valor_1)+" * "+ str(c.valor_2)+"="+ str(c.resultado))
+
+@app.route('/divicion/<int:valor_1>/<int:valor_2>')
+def divicion(valor_1, valor_2):
+    c = Calculadora()
+    c.valor_1 = valor_1
+    c.valor_2 = valor_2
+    c.dividir()
+    return (str(c.valor_1)+" / "+ str(c.valor_2)+"="+ str(c.resultado))
+
+@app.route('/operacion/<operador>/<int:valor_1>/<int_valor_2>')
+def operacion(operador, valor_1, valor_2):
+    if operador == "+":
+        return suma(valor_1, valor_2)
+    elif operador == "-"
+        return resta(valor_1, valor_2)
+    elif operador == "*"
+        return multiplicacion(valor_1, valor_2)
+    elif operador == "/"
+        return divicion(valor_1, valor_2)
+    else:
+        return hola()
 
 if __name__ == '_main_':
     app.run(debug =  True)
